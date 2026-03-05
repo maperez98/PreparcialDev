a8b4ed6b1299841ce7abc6acd3cf1f14550142cf
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-03-05 15:19:17 -0500

    Para prueba de que funciona git

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/prueba")
+def read_root():
+    return {"Hello": "Esta es la prueba para  verificar que funciona lanzar el localhost"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
